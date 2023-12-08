6b8d72e99015acef9434f9e15eb5df10d4fa2ab7
Alexey Cherkashin <goodniceweb@gmail.com>
2023-12-09 01:34:07 +0600

    Auto-adjust destination + small tweaks
    Auto-adjust destination: prepend "//" (instead of hardcoding protocol)
    when protocol was missing.
    
    Tweaks:
    - more validations
    - re-arrange file layout a bit

--- app/models/qr_code.rb
+++ app/models/qr_code.rb
@@ -2,9 +2,11 @@ class QRCode < ApplicationRecord
   mount_uploader :svg, SvgQRUploader
   mount_uploader :png, PngQRUploader
 
-  validates :destination, presence: true
+  validates :destination, :path_alias, presence: true
+  validates :path_alias, uniqueness: true
 
-  before_save :ensure_path_alias
+  before_validation :ensure_path_alias
+  before_create :ensure_correct_destination
   after_create :ensure_svg
   after_create :ensure_png
 
@@ -19,18 +21,10 @@ class QRCode < ApplicationRecord
     self.path_alias = potential_path_alias
   end
 
-  def store_file(type, content)
-    tempfile = Tempfile.new(["qr_code-#{path_alias}", ".#{type}"])
-    tempfile.binmode
-    tempfile.write(content.public_send(:"as_#{type}"))
-    tempfile.close
+  def ensure_correct_destination
+    return if destination.start_with?("http", "//")
 
-    File.open(tempfile.path) do |file|
-      public_send(:"#{type}=", file)
-      save!
-    end
-
-    File.delete(tempfile.path)
+    self.destination = "//#{destination}"
   end
 
   def ensure_svg
@@ -43,6 +37,20 @@ class QRCode < ApplicationRecord
     return if png.present?
 
     store_file(:png, generete_qr_code)
+  end
+
+  def store_file(type, content)
+    tempfile = Tempfile.new(["qr_code-#{path_alias}", ".#{type}"])
+    tempfile.binmode
+    tempfile.write(content.public_send(:"as_#{type}"))
+    tempfile.close
+
+    File.open(tempfile.path) do |file|
+      public_send(:"#{type}=", file)
+      save!
+    end
+
+    File.delete(tempfile.path)
   end
 
   def generete_qr_code

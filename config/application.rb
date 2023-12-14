ac471786f5b2d6c474731cd41f3ea8cf111eb01f
Alexey Cherkashin <goodniceweb@gmail.com>
2023-12-15 00:31:47 +0600

    Rename service: Qubikl

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module QRCodeGenerator
+module Qubikl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0

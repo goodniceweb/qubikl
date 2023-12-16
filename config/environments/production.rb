e8c2d77e817875c77e56b5229801793adb10b42f
Alexey Cherkashin <goodniceweb@gmail.com>
2023-12-17 01:18:39 +0600

    Add default host and port temporary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,3 +113,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+Rails.application.routes.default_url_options[:host] = "bot.goodniceweb.me"
+Rails.application.routes.default_url_options[:port] = 3001
